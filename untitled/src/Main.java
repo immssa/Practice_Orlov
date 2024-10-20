20f9281897bd66d8846ace1cdd78ac9b55149b99
immssa <safronova2461@gmail.com>
2024-10-20 19:58:58 +0500

    com

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -15,8 +15,15 @@ public class Main {
         Scanner scanner3 = new Scanner(System.in);
         System.out.println(scanner3.nextInt() + 1);
 
-        //Task 4 (ID:0942)
-
+        //Task 4 (ID:0766)
+        Scanner scanner4 = new Scanner(System.in);
+        int N = scanner4.nextInt();
+        int M = scanner4.nextInt();
+        int K = scanner4.nextInt();
+        if (N * M >= K)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
 
         //Task 5 (ID:0025)
         Scanner scanner5 = new Scanner(System.in);
@@ -28,5 +35,15 @@ public class Main {
             System.out.println("<");
         else
             System.out.println("=");
+
+        //Task 6 (ID:0195)
+        Scanner scanner6 = new Scanner(System.in);
+        System.out.println(scanner6.nextInt() * scanner6.nextInt() * scanner6.nextInt() * 2);
+
+        //Task 7 (ID:0773)
+        Scanner scanner7 = new Scanner(System.in);
+        int k = scanner7.nextInt();
+        int m = scanner7.nextInt();
+        System.out.println(k * k * m);
     }
 }
